5706dedb55d6b78b61d8e6f35eca9b008ac4bef1
Cristopher Martínez <128097127+CristopherPht@users.noreply.github.com>
2023-08-27 03:37:47 -0700

    Delete Sum directory

--- Sum/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-        public static void main(String[] args) {
-            int num1 = 5;
-            int num2 = 2;
-            int result;
-            result = num1 + num2;
-            System.out.println("El resultado sería = " + result);
-        }
-}
