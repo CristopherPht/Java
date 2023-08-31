aaca3302f433a54ad540e8110a0f2e2df824f86b
CristopherPht <cristopher.martinez.glez@gmail.com>
2023-08-30 21:59:41 -0700

    Resultado esperado

--- 07 ListaDePrecios.java
+++ 07 ListaDePrecios.java
@@ -1,35 +1,21 @@
 import java.lang.reflect.Array;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 class ListaDePrecios{
 
-    private static final Object[] Array = null;
-
     public static void main(String[] args){
-    int cantidadProductos;
-    int precioQuesadillas, precioRefrescos, precioJugos;
-     System.out.println("Ingresa la cantidad de productos: ");
-        Scanner cantidad = new Scanner(System.in);
-        cantidadProductos = cantidad.nextInt();
-        Array[cantidadProductos];
-     System.out.println("\nIngresa el precio de las quesadillas: ");
-        Scanner precioQ = new Scanner(System.in);
-        precioQuesadillas = precioQ.nextInt();
-    System.out.println("\nIngresa el precio de los refrescos: ");
-        Scanner precioR = new Scanner(System.in);
-        precioRefrescos = precioR.nextInt();
-    System.out.println("\nIngresa el precio de los jugos: ");
-        Scanner precioJ = new Scanner(System.in);
-        precioJugos = precioJ.nextInt();
-    System.out.println("Cantidad       " + "Quesadillas      " + "Refrescos     "+ "     Jugos");
-    for (int i = 1; i < cantidadProductos; i++) {
-
-        
-    }
-
-
-
-
-        
+        int cantidadProductos, precioQuesadillas, precioRefrescos, precioJugos, pantalla;
+        Scanner entrada = new Scanner(System.in);
+        cantidadProductos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de productos: "));
+        int[] productos = new int[cantidadProductos];
+        precioQuesadillas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el precio de las quesadillas: "));
+        precioRefrescos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el precio de los refrescos: "));
+        precioJugos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de productos: "));
+        System.out.println("Cantidad       " + "Quesadillas      " + "Refrescos     "+ "     Jugos");
+        for (int i = 0; i < productos.length; i++) {
+            System.out.println((i+1) + "              " + (precioQuesadillas * i) + "                " + (precioRefrescos * i) + "                " + (precioJugos * i));
+        }        
     }
 }
