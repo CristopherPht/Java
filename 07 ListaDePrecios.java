f016f646dcbce9c06acb73a7a8a9c00c10bbe8bd
CristopherPht <cristopher.martinez.glez@gmail.com>
2023-08-30 20:20:01 -0700

    Se comenzó el código de la Lista de Precios

--- 07 ListaDePrecios.java
+++ 07 ListaDePrecios.java
@@ -1,5 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 class ListaDePrecios{
+
+    private static final Object[] Array = null;
+
     public static void main(String[] args){
+    int cantidadProductos;
+    int precioQuesadillas, precioRefrescos, precioJugos;
+     System.out.println("Ingresa la cantidad de productos: ");
+        Scanner cantidad = new Scanner(System.in);
+        cantidadProductos = cantidad.nextInt();
+        Array[cantidadProductos];
+     System.out.println("\nIngresa el precio de las quesadillas: ");
+        Scanner precioQ = new Scanner(System.in);
+        precioQuesadillas = precioQ.nextInt();
+    System.out.println("\nIngresa el precio de los refrescos: ");
+        Scanner precioR = new Scanner(System.in);
+        precioRefrescos = precioR.nextInt();
+    System.out.println("\nIngresa el precio de los jugos: ");
+        Scanner precioJ = new Scanner(System.in);
+        precioJugos = precioJ.nextInt();
+    System.out.println("Cantidad       " + "Quesadillas      " + "Refrescos     "+ "     Jugos");
+    for (int i = 1; i < cantidadProductos; i++) {
+
+        
+    }
+
+
+
+
         
     }
 }
